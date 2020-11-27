9f565a2e7f31229d017f8dea36f05c9456feda09
Pj Metz <65838556+MetzinAround@users.noreply.github.com>
2020-11-27 12:49:04 -0500

    Add project files.

---
+++ PerfectTurkey/Pages/Index.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Web.Mvc;
+
+
+namespace PerfectTurkey.Pages
+{ 
+    public class IndexModel : PageModel
+    {
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+          
+
+        }
+        readonly MakeATurkey turkey = new MakeATurkey();
+
+        public string SubmissionResult { get; private set; } = "";
+        public async Task OnPostSubmit(string weight)
+        {
+
+            double doubleWeight;
+
+            if (weight is null || !double.TryParse(weight, out doubleWeight))
+            {
+                SubmissionResult = "Please enter a number";
+            }
+            else
+            {
+                var weighted = Convert.ToDouble(weight);
+                
+                try
+                {
+                    await Task.Delay(1000);
+                    SubmissionResult = turkey.Recipe(weighted);
+                }
+                catch
+                {
+                    SubmissionResult = "TURKEY, WHAT ARE YOU DOING?";
+                }
+            }
+        }
+    }
+}
---
+++ PerfectTurkey/Pages/MakeATurkey.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerfectTurkey.Pages
+{
+    public class MakeATurkey
+    {
+        public static double weight
+        { get; set; }
+
+        //cups
+        double salted = weight * .05;
+        //gallons
+        double water = weight * .66;
+        //cups
+        double brwnSugar = weight * .13;
+
+        double shallots = weight * .2;
+
+        double garlic = weight * .4;
+        //tbs
+        double peppercorns = weight * .13;
+        //tbs
+        double juniper = weight * .13;
+        //tbs
+        double rosemary = weight * .13;
+        //tbs
+        double thyme = weight * .06;
+        //hours
+        double brineTime = weight * 2.4;
+        //minutes
+        double cookTime = weight * 15;
+
+        string finalRecipe = "";
+
+        public string Recipe(double weight)
+        {
+            //cups
+            double salted = weight * .05;
+            //gallons
+            double water = weight * .66;
+            //cups
+            double brwnSugar = weight * .13;
+
+            double shallots = weight * .2;
+
+            double garlic = weight * .4;
+            //tbs
+            double peppercorns = weight * .13;
+            //tbs
+            double juniper = weight * .13;
+            //tbs
+            double rosemary = weight * .13;
+            //tbs
+            double thyme = weight * .06;
+            //hours
+            double brineTime = weight * 2.4;
+            //minutes
+            double cookTime = weight * 15;
+
+            finalRecipe = $" * You'll need: \n - {salted:N} cups of Salt \n - {water:N} gallons of water \n - {brwnSugar:N} cups of brown sugar \n - {shallots:N} shallots \n" +
+                $" - {garlic:N} cloves of garlic \n - {peppercorns:N} tablespoons of whole black peppercorns \n - {juniper:N} tablespoons of dried juniper berries \n" +
+                $" - {rosemary:N} tablespoons of fresh rosemary \n" +
+                $"- {thyme:N} tablespoons of thyme \n - {brineTime:N} hours soaked in brine \n - {(cookTime / 60):N} hours baked in the oven.";
+
+            return finalRecipe;
+        }
+    }
+}
